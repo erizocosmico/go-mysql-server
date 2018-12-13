ee901cc99ad5c73ff8ea406e9558b8b28a0d6e88
Miguel Molina <miguel@erizocosmi.co>
2018-12-13 15:18:05 +0100

    analyzer: remove subqueryBarrier
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

--- sql/analyzer/prune_columns.go
+++ sql/analyzer/prune_columns.go
@@ -29,12 +29,7 @@ func pruneColumns(ctx *sql.Context, a *Analyzer, n sql.Node) (sql.Node, error) {
 
 	findUsedColumns(columns, n)
 
-	n, err := addSubqueryBarriers(n)
-	if err != nil {
-		return nil, err
-	}
-
-	n, err = pruneUnusedColumns(n, columns)
+	n, err := pruneUnusedColumns(n, columns)
 	if err != nil {
 		return nil, err
 	}
@@ -81,12 +76,7 @@ func pruneSubqueryColumns(
 
 	findUsedColumns(columns, n.Child)
 
-	node, err := addSubqueryBarriers(n.Child)
-	if err != nil {
-		return nil, err
-	}
-
-	node, err = pruneUnusedColumns(node, columns)
+	node, err := pruneUnusedColumns(n.Child, columns)
 	if err != nil {
 		return nil, err
 	}
@@ -126,17 +116,6 @@ func findUsedColumns(columns usedColumns, n sql.Node) {
 	})
 }
 
-func addSubqueryBarriers(n sql.Node) (sql.Node, error) {
-	return n.TransformUp(func(n sql.Node) (sql.Node, error) {
-		sq, ok := n.(*plan.SubqueryAlias)
-		if !ok {
-			return n, nil
-		}
-
-		return &subqueryBarrier{sq}, nil
-	})
-}
-
 func pruneSubqueries(
 	ctx *sql.Context,
 	a *Analyzer,
@@ -144,12 +123,12 @@ func pruneSubqueries(
 	parentColumns usedColumns,
 ) (sql.Node, error) {
 	return n.TransformUp(func(n sql.Node) (sql.Node, error) {
-		barrier, ok := n.(*subqueryBarrier)
+		subq, ok := n.(*plan.SubqueryAlias)
 		if !ok {
 			return n, nil
 		}
 
-		return pruneSubqueryColumns(ctx, a, barrier.SubqueryAlias, parentColumns)
+		return pruneSubqueryColumns(ctx, a, subq, parentColumns)
 	})
 }
 
@@ -289,12 +268,4 @@ func shouldPruneExpr(e sql.Expression, cols usedColumns) bool {
 	}
 
 	return true
-}
-
-type subqueryBarrier struct {
-	*plan.SubqueryAlias
-}
-
-func (b *subqueryBarrier) TransformUp(f sql.TransformNodeFunc) (sql.Node, error) {
-	return f(b)
 }
